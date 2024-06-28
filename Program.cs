68755c1f7a666348471a570c19caf3434a5265ad
Imronbek0250 <sultanovimron0@gmail.com>
2024-06-29 00:02:34 +0500

    Add C# Project File

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Bu dastur .NET haqida
+*/
+using System;
+using System.IO;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks.Dataflow;
+using System.Xml;
+
+namespace Calculator
+{
+    class Program 
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("Enter values");
+            Console.Write("Enter number");
+            string firstUserValue = Console.ReadLine();
+            Console.Write("Second number");
+            string secondUserValue = Console.ReadLine();
+
+            Console.WriteLine("Converting values in progress....");
+            decimal firstNumber = Convert.ToDecimal(firstUserValue);
+            decimal secondNumber = Convert.ToDecimal(secondUserValue);
+
+            Console.WriteLine("Here are your results");
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
+            Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
+            Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
+
+        }
+    }
+}
