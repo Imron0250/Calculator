1e24be221a9db4fe1839c76777e42524bb9c6bcd
Imronbek0250 <sultanovimron0@gmail.com>
2024-06-30 22:38:58 +0500

    Refactor Swtich

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 using System.Threading;
 
 namespace Calculator
@@ -8,55 +10,32 @@ namespace Calculator
         static void Main()
         {
 
-            // Console.WriteLine("Hello, pls enter number below ");
-            // Console.Write("First number: ");
-            // string firstUserNum = Console.ReadLine();
-            // Console.Write("Second Number: ");
-            // string secondUserNum = Console.ReadLine();
-
-            // Console.WriteLine("Information is progressing....");
-            // decimal firstNumber = Convert.ToDecimal(firstUserNum);
-            // decimal secondNumber = Convert.ToDecimal(secondUserNum);
-
-            // Console.WriteLine("Here are your results");
-            // Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
-            // Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");
-            // Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");
-            // Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");
-            // Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");
-
             Console.Write("Pls enter first number: ");
             int firstNum = Convert.ToInt32(Console.ReadLine());
             Console.Write("Pls enter the medthod: ");
             string userMethod = Console.ReadLine();
             Console.Write("Pls enter second number: ");
             int secondNum = Convert.ToInt32(Console.ReadLine());
+            string template = $"{firstNum} {userMethod} {secondNum} = ";
+            decimal result = userMethod switch
+            {
+                "+" => firstNum + secondNum,
+                "-" => firstNum - secondNum,
+                "*" => firstNum * secondNum,
+                "/" => firstNum / secondNum,
+                "%" => firstNum % secondNum,
+                _ => 0 
+            };
 
-            if (userMethod == "+")
-            {
-                Console.WriteLine($"{firstNum} + {secondNum} = {firstNum + secondNum}  ");
+            Console.WriteLine($"{template} {result}");
+
+
+
+
+                
+                
+                
             
-            }
-            else if (userMethod == "-")
-            {
-                Console.WriteLine($"{firstNum} - {secondNum} = {firstNum - secondNum}  ");
-            }
-            else if (userMethod == "*")
-            {
-                Console.WriteLine($"{firstNum} * {secondNum} = {firstNum * secondNum}  ");
-            }
-            else if (userMethod == "/")
-            {
-                Console.WriteLine($"{firstNum} / {secondNum} = {firstNum / secondNum}  ");
-            }
-            else if (userMethod == "%")
-            {
-                Console.WriteLine($"{firstNum} % {secondNum} = {firstNum % secondNum}  ");
-            }
-            else 
-            {
-                Console.WriteLine("We dont have this method");
-            }
 
             
 
