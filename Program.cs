72990549c5a990a712347ebdd666b0dff2ee6352
Imronbek0250 <sultanovimron0@gmail.com>
2024-07-02 14:31:00 +0500

    The Fight Club Membership Survey

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Threading;
 
@@ -9,35 +10,47 @@ namespace Calculator
     {
         static void Main()
         {
+           
+           Console.WriteLine("Welcom to the fight club");
+           Console.Write("Pleas enter your name: ");
+           string userName = Console.ReadLine();
+           Console.Write("Pleas enter your second name: ");
+           string userLname = Console.ReadLine();
+            Console.Write("Pleas enter your age: ");
+           int userAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Pls enter first number: ");
-            int firstNum = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Pls enter the medthod: ");
-            string userMethod = Console.ReadLine();
-            Console.Write("Pls enter second number: ");
-            int secondNum = Convert.ToInt32(Console.ReadLine());
-            string template = $"{firstNum} {userMethod} {secondNum} = ";
-            decimal result = userMethod switch
+           string  fullName = $"{userName} {userLname} and your age is {userAge}";
+           if (userAge >= 18)
+           {
+
+            Console.WriteLine($"Welcom to the fight club {fullName}");
+            Console.Write("Do you want to now abote rules of fight club yes/no ?: ");
+            string usersAnswer = Console.ReadLine();
+            switch (usersAnswer)
             {
-                "+" => firstNum + secondNum,
-                "-" => firstNum - secondNum,
-                "*" => firstNum * secondNum,
-                "/" => firstNum / secondNum,
-                "%" => firstNum % secondNum,
-                _ => 0 
-            };
+                case "yes":
+                    Console.WriteLine("The first ruel of fight club, You do not talk about");
+                    Console.WriteLine("The second ruel of fight club, You DO NOT talk about FIGHT CLUB");
+                    Console.WriteLine("The third ruel of fight club, If someone says 'STOP' or goes limp, taps out the fight is over");
+                    Console.WriteLine("The fourth ruel of fight club,  Only two guys to a fight");
+                break;
 
-            Console.WriteLine($"{template} {result}");
+                case "no":
+                    Console.WriteLine("How you gonna bee a membor of fight club without knowing about rules you mother facker, go fack out form here");
+                break;
 
+                default :
+                    Console.WriteLine("Nigga we have only 2 options yes/no, you got it?");
+                break;
+            }
+           }
+           else 
+           {
+            Console.WriteLine("Bro sorry but your age is not enaough");
+           }
+           
 
-
-
-                
-                
-                
-            
-
-            
+           
 
         }
     }
